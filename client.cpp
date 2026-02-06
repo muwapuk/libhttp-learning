521a7860e364592b7550cd8321555b91d9cf01f4
Muwapuk <misharik1010@gmail.com>
2026-02-06 11:27:32 +1000

    Added host and name info printing

--- client.cpp
+++ client.cpp
@@ -34,6 +34,27 @@ int main(int argc, char *argv[])
 	exit(1);
     }
 
+    sockaddr_in peer_addr;
+    socklen_t peer_addr_len = sizeof(peer_addr);
+    getpeername(sockfd, (sockaddr*)&peer_addr, &peer_addr_len);
+
+    char hoststr[NI_MAXHOST];
+    char servstr[NI_MAXSERV];
+    char hostnamestr[256];
+    
+    gethostname(hostnamestr, 256);
+    std::cout << "Your computer name is: " << hostnamestr << std::endl;
+
+    err = getnameinfo((sockaddr *)&peer_addr,
+    		      peer_addr_len,
+    		      hoststr,
+    		      sizeof(hoststr),
+    		      servstr,
+    		      sizeof(servstr),
+    		      NI_NUMERICHOST | NI_NUMERICSERV);
+
+    std::cout << "The peer addr is: " << hoststr << ':' << servstr << std::endl;
+
     char buf[256];
     recv(sockfd, buf, sizeof(char)*256, 0);
 
