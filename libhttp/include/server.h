9d0ce07b3c68924d47e091dbf2d2c89bdc9c8f8c
Muwapuk <misharik1010@gmail.com>
2026-02-25 16:17:12 +1000

    Server connection queue with poll()

--- libhttp/include/server.h
+++ libhttp/include/server.h
@@ -1,32 +1,39 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include "networkfuncs.h"
-
-#include <iostream>
-#include <cstring>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <string>
 #include <sys/socket.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <poll.h>
+#include <vector>
 
 namespace libhttp 
 {
 class Server
-{
-    int sockfd;
+{	
+	const int MAX_CONNECTIONS = 15;
+
+    int listen_sockfd;
     std::string ip;
     std::string hostname;
     int port;
+
+	int poll_descriptors_count;
+	size_t poll_descriptors_size = 4;
+	std::vector<pollfd> poll_descriptors;
+
     // Returns socket fd
     int create_socket(const std::string &host, int port,
 			   		  /*int address_family,*/ int socket_flags = 0);    
 	bool fill_socket_info(const std::string &host);
-	bool handle_connection(int client_socket);
+	void process_descriptors(int fd_count);
+	void handle_new_connection();
+	void handle_client_data(int pollfd_index);
+	void add_to_poll_descriptors(int fd);
 
 public:
     bool listen(const std::string &host, int port);
--- libhttp/src/server.cpp
+++ libhttp/src/server.cpp
@@ -1,13 +1,51 @@
 #include "server.h"
 
+#include "networkfuncs.h"
+
+#include <iostream>
+#include <cstring>
+#include <string.h>
+
 using namespace libhttp;
 
+bool Server::listen(const std::string &host, int port)
+{
+	listen_sockfd = create_socket(host, port);
+	if(listen_sockfd < 0) {
+		std::cerr << "Bad socket" << std::endl;
+		return 1;
+	}
+	fill_socket_info(host);
+	std::cout << "Listening on: " << ip << ':' << port << std::endl;
+
+	if(::listen(listen_sockfd, 10) == -1) {
+		std::cerr << "listen: " << std::strerror(errno) << std::endl;
+		return 1;
+	}
+	
+	poll_descriptors.reserve(poll_descriptors_size);
+	pollfd listen_pollfd;
+	listen_pollfd.fd = listen_sockfd;
+	listen_pollfd.events = POLLIN;
+	poll_descriptors.push_back(listen_pollfd);
+
+	for(;;) {
+		int poll_count = poll(poll_descriptors.data(), poll_descriptors.size(), -1);
+		if(poll_count == -1) {
+			std::cerr << "Poll: " << std::strerror(errno) << std::endl;
+			/* ERROR HANDING */
+			break;
+		}
+		process_descriptors(poll_count);
+	}
+	return 0;
+}
 int Server::create_socket(const std::string &host,
 						  int port,
 						  /*int address_family,*/ 
 						  int socket_flags)
 {
-	int sockfd;
+	int listen_sockfd;
 	addrinfo hints;
 
 	memset(&hints, 0, sizeof hints);
@@ -17,75 +55,91 @@ int Server::create_socket(const std::string &host,
 	std::string portstr = std::to_string(port);
 	if(0 != getaddrinfo(host.c_str(), portstr.c_str(), &hints, &servinfo)) {
 	    std::cerr << "getaddrinfo() error: " << std::strerror(errno) << std::endl; 
-	    exit(1);
+		return -1;
 	}
-	if(-1 == (sockfd = socket(servinfo->ai_family,
+	if(-1 == (listen_sockfd = socket(servinfo->ai_family,
 							  servinfo->ai_socktype,
 							  servinfo->ai_protocol))) {
-		    std::cerr << "socket() error: " << std::strerror(errno) << std::endl; 
+		std::cerr << "socket() error: " << std::strerror(errno) << std::endl; 
+		return -1;
 	}
 	// Reuse port
 	int yes=1;
-	if(-1 == setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes)) { 
+	if(-1 == setsockopt(listen_sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes)) { 
 	    std::cerr << "setsockoopt() error: " << std::strerror(errno) << std::endl; 
+		return -1;
 	}
-	if(-1 == bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen)) {
+	if(-1 == bind(listen_sockfd, servinfo->ai_addr, servinfo->ai_addrlen)) {
 	    std::cerr << "bind() error: " << std::strerror(errno) << std::endl; 
+		return -1;
 	}
 	freeaddrinfo(servinfo);
-	return sockfd;
+
+	return listen_sockfd;
 }
+
 bool Server::fill_socket_info(const std::string &host)
 {
 	sockaddr_storage addr;
 	socklen_t addr_len = sizeof(addr);
-	::getsockname(sockfd, (sockaddr*)&addr, &addr_len);
+	::getsockname(listen_sockfd, (sockaddr*)&addr, &addr_len);
 
 	hostname = host;
 	ip = get_ip_string((sockaddr*)&addr);
 
 	return 0;
 }
-bool Server::handle_connection(int client_socket)
+void Server::process_descriptors(int fd_count)
 {
-	if(!fork()) {
-	    close(sockfd);
-	    if(send(client_socket, "Hello, world!", 13, 0) == -1)
-		std::cerr << "Send: " << std::strerror(errno) << std::endl;
-	    close(client_socket);
-	    exit(0);
+	for(int i = 0; i < fd_count; i++) {
+		if(poll_descriptors[i].revents & (POLLIN | POLLHUP)) {
+			if(poll_descriptors[i].fd == listen_sockfd)
+				handle_new_connection();
+			else
+				handle_client_data(i);
+		}
 	}
-	close(client_socket);
-	return 0;
 }
-bool Server::listen(const std::string &host, int port)
+void Server::handle_new_connection()
 {
-	sockfd = create_socket(host, port);
-	if(sockfd < 0) {
-		std::cerr << "Bad socket" << std::endl;
-		exit(1);
-	}
-	fill_socket_info(host);
-	std::cout << "Listening on: " << ip << ':' << port << std::endl;
-
-	if(::listen(sockfd, 10) == -1) {
-		std::cerr << "listen: " << std::strerror(errno) << std::endl;
-	}
-	
-
 	sockaddr_storage client_addr;
 	socklen_t client_addr_size = sizeof(client_addr);
-	for(;;) {
-		int client_sock = accept(sockfd,
+	int client_sock = accept(listen_sockfd,
 								 (sockaddr*)&client_addr,
 								 &client_addr_size);
-		if(client_sock == -1) {
-		    std::cerr << "Connection accept: " << std::strerror(errno) << std::endl;
-			exit(1);
-		    continue;
+	if(client_sock == -1) {
+	    std::cerr << "Connection accept: " << std::strerror(errno) << std::endl;
+		/* ERROR HANDING */
+		return;
+	}
+	std::cout << "Accepted connection on descriptor " << client_sock << ": " << get_ip_string((sockaddr*)&client_addr) << std::endl;
+	add_to_poll_descriptors(client_sock);
+}
+void Server::add_to_poll_descriptors(int fd)
+{
+	pollfd newfd;
+	newfd.fd = fd;
+	newfd.events = POLLIN;
+	newfd.revents = 0;
+
+	poll_descriptors.push_back(newfd);
+}
+void Server::handle_client_data(int pollfd_index)
+{
+	char buf[256];
+	int client_fd = poll_descriptors[pollfd_index].fd; 
+
+	int nbytes = recv(client_fd, buf, sizeof buf, 0); 
+
+	if(nbytes <= 0) {
+		if(nbytes == 0) {
+			std::cout << "Server: socket " << client_fd << " closed" << std::endl;
 		} else {
-			std::cout << "Accepted connection: " << get_ip_string((sockaddr*)&client_addr) << std::endl;
+			std::cerr << "recv: " << std::strerror(errno) << std::endl;
 		}
-		handle_connection(client_sock);
+		close(client_fd);
+		poll_descriptors.erase(poll_descriptors.begin() + pollfd_index);
+	} else {
+		std::cout << "Server: recv from fd " << client_fd << ':' << buf << std::endl;	
 	}
 }
